4c2a6b2ad25282d4525a205c33bc300d5e2cbbe6
jp-lourenco <59509134+jp-lourenco@users.noreply.github.com>
2021-01-15 15:54:08 +0000

    Add app.js

--- App.js
+++ App.js
@@ -2,15 +2,31 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Home from './src/pages/Home';
+import Login from './src/pages/Login';
 import Scan from './src/pages/Scan';
+import Create from './src/pages/Create';
 
 const Stack = createStackNavigator();
 
 export default function App() {
     return (
-        <NavigationContainer initialRouteName="Scan">
+        <NavigationContainer initialRouteName="Home">
             <Stack.Navigator>
-                <Stack.Screen name="Scan" component={Scan} />
+                <Stack.Screen
+                    name="Home"
+                    component={Home}
+                    options={{
+                        headerShown: false,
+                    }}
+                />
+                <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen
+                    name="Scan"
+                    component={Scan}
+                    options={{ headerLeft: false }}
+                />
+                <Stack.Screen name="Create" component={Create} />
             </Stack.Navigator>
         </NavigationContainer>
     );
